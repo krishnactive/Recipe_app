9cfeb53cce73d361959bf080e30ea2486394d0de
krishna <krishnactive@gmail.com>
2024-09-07 14:34:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+
+//themealdb api is used here 
+//function to get recipes
+
+const fetchRecipes = async (query)=>{
+    const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const response =await data.json();
+    console.log(response);
+}
+
+
+
+searchBox.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const searchInput = searchBox.value.trim();
+    fetchRecipes();
+});
+
