eb58d50563120c1fc753c6618ec031a100c34c8f
krishna <krishnactive@gmail.com>
2024-09-07 17:47:02 +0530

    adding api

--- script.js
+++ script.js
@@ -9,14 +9,22 @@ const recipeContainer = document.querySelector('.recipe-container');
 const fetchRecipes = async (query)=>{
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response =await data.json();
-    console.log(response);
+    
+    response.meals.forEach(meal=>{
+        const recipeDiv = document.createElement("div");//creating div that is element
+        recipeDiv.classList.add("recipe"); //adding classes to newly formed div
+        recipeDiv.innerHTML = ` //defining inner html
+        <img src = "${meal.strMealThumb}">
+        `
+        recipeContainer.appendChild(recipeDiv); //adding recipe which is created get add to the recipe container
+    })
 }
 
 
 
-searchBox.addEventListener('click', (e)=>{
+searchBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     const searchInput = searchBox.value.trim();
-    fetchRecipes();
+    fetchRecipes(searchInput);
 });
 
